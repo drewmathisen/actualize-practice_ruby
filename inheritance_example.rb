632c898f514943df061543600055b0700db02c34
d.mathisen <drew.a.mathisen@gmail.com>
2021-10-06 18:51:35 -0600

    Created a bike class and a car class and then used the DRY method to clean up the code

---
+++ inheritance_example.rb
@@ -0,0 +1,40 @@
+class Transport
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Transport
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+
+end
+
+class Bike < Transport
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+
+end
+
+car1 = Car.new
+bike1 = Bike.new
+
+bike1.ring_bell
+car1.honk_horn
