7e131669e4a840ed15927b757537bbf9bd7b8056
d.mathisen <drew.a.mathisen@gmail.com>
2021-10-07 19:26:44 -0600

    hid API key

--- web_request.rb
+++ web_request.rb
@@ -1,3 +1,5 @@
 require 'http'
 
-pp response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json").parse(:json)
+pp response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=metric").parse(:json)
+
+
